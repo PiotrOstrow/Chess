7c84825b9cc4bc9f39c6bad507bca5b59d689a39
Sofia Sinani <sofsin1208@gmail.com>
2020-11-26 18:48:20 +0100

    Updated Rook

--- src/main/java/com/example/game/pieces/Rook.java
+++ src/main/java/com/example/game/pieces/Rook.java
@@ -13,6 +13,26 @@ public class Rook extends ChessPiece {
 
     @Override
     protected boolean canMove(Game game, int x, int y) {
+        int direction;
+        if(position.getX() == x && position.getY() != y) { // if X is the same, and Y is different -> move up and down
+            // figure out direction of the move
+            direction = position.getY() < y ? 1 : -1;
+            // check every piece on the way, starting from position + one step ahead
+            for(int y1 = position.getY() + direction; y1 != y; y1 += direction) {
+                // if there is a piece, can not move
+                if(game.getPiece(x, y1) != null)
+                    return false;
+            }
+            // no piece on the way, can move
+            return true;
+        } else if(position.getX() != x && position.getY() == y) {
+            direction = position.getX() < x ? 1 : -1;
+            for(int x1 = position.getX() + direction; x1 != x; x1 += direction) {
+                if(game.getPiece(x1, y) != null)
+                    return false;
+            }
+            return true;
+        }
         return false;
     }
 
