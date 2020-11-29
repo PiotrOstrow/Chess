d28a212541c1265ffe73188cdba75cf2674ea61d
Peter Nordström <70512860+PNordstrom81@users.noreply.github.com>
2020-11-29 17:36:30 +0100

    Update Knight.java

--- src/main/java/com/example/game/pieces/Knight.java
+++ src/main/java/com/example/game/pieces/Knight.java
@@ -27,12 +27,44 @@ public class Knight extends ChessPiece {
     public String getImagePath() {
         if(color == Color.WHITE)
             return "Pixel_Art_Chess_DevilsWorkshop_V03\\chess\\white_knight.png";
+        else
         return "Pixel_Art_Chess_DevilsWorkshop_V03\\chess\\black_knight.png";
     }
+   
+    private boolean isValidPosition(Game game, int x, int y) {
+        return x >= 0 && y >= 0 && x < 8 && y < 8
+                && (game.getPiece(x, y) == null || game.getPiece(x, y).color != this.color);
+
+    }
+
 
     @Override
     public List<Position> getPossibleMoves(Game game) {
-        return null;
+        List<Position> list = new ArrayList<>();
+
+
+        if(isValidPosition(game, position.getX() + 1, position.getY() + 2))
+            list.add(new Position(position.getX() + 1, position.getY() + 2));
+        if(isValidPosition(game, position.getX() + 2, position.getY() + 1))
+            list.add(new Position(position.getX() + 2, position.getY() + 1));
+        if(isValidPosition(game, position.getX() - 1, position.getY() - 2))
+            list.add(new Position(position.getX() - 1, position.getY() - 2));
+        if(isValidPosition(game, position.getX() - 2, position.getY() - 1))
+            list.add(new Position(position.getX() - 2, position.getY() - 1));
+        if(isValidPosition(game, position.getX() - 1, position.getY() + 2))
+            list.add(new Position(position.getX() - 1, position.getY() + 2));
+        if(isValidPosition(game, position.getX() + 1, position.getY() - 2))
+            list.add(new Position(position.getX() + 1, position.getY() - 2));
+        if(isValidPosition(game, position.getX() + 2, position.getY() - 1))
+            list.add(new Position(position.getX() + 2, position.getY() - 1));
+
+
+        return list;
     }
+
+
+
+
+
 }
 
