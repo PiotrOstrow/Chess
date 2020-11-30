97f6178fbe38697a0dce7b4dbf8cf64ee91532c3
Sofia Sinani <sofsin1208@gmail.com>
2020-11-30 20:04:10 +0100

    not ready yet

--- src/main/java/com/example/main/App.java
+++ src/main/java/com/example/main/App.java
@@ -4,9 +4,12 @@ import com.example.game.Color;
 import com.example.game.Game;
 import com.example.ui.CapturedPiecesBar;
 import com.example.ui.GameBoard;
+import com.example.ui.MainMenu;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,24 +17,32 @@ public class App extends Application {
 	private GameBoard gameBoard;
 	private CapturedPiecesBar topBar;
 	private CapturedPiecesBar bottomBar;
+	private MainMenu mainMenu;
 
 	@Override
 	public void start(Stage primaryStage) {
-		gameBoard = new GameBoard();
 
-		topBar = new CapturedPiecesBar(Color.BLACK);
-		bottomBar = new CapturedPiecesBar(Color.WHITE);
+		{ // Main menu
 
-		BorderPane borderPane = new BorderPane();
-		borderPane.setCenter(gameBoard);
-		borderPane.setTop(topBar);
-		borderPane.setBottom(bottomBar);
+		}
 
-		Scene scene = new Scene(borderPane, 600, 600);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		{ // Game
+			gameBoard = new GameBoard();
 
-		startGame(new Game());
+			topBar = new CapturedPiecesBar(Color.BLACK);
+			bottomBar = new CapturedPiecesBar(Color.WHITE);
+
+			BorderPane borderPane = new BorderPane();
+			borderPane.setCenter(gameBoard);
+			borderPane.setTop(topBar);
+			borderPane.setBottom(bottomBar);
+
+			Scene scene = new Scene(borderPane, 600, 600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			startGame(new Game());
+		}
 	}
 
 	private void startGame(Game game){
