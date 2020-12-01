4fd6c85a66b53eb00c93da26ab86d1b4ace4eb8f
Sofia Sinani <sofsin1208@gmail.com>
2020-12-01 16:11:20 +0100

    main_menu klar

--- src/main/java/com/example/main/App.java
+++ src/main/java/com/example/main/App.java
@@ -6,7 +6,12 @@ import com.example.ui.CapturedPiecesBar;
 import com.example.ui.GameBoard;
 import com.example.ui.MainMenu;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -22,27 +27,30 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		{ // Main menu
+		mainMenu = new MainMenu();
+		gameBoard = new GameBoard();
+		gameBoard.setEffect(new GaussianBlur(10));
 
-		}
+		topBar = new CapturedPiecesBar(Color.BLACK);
+		bottomBar = new CapturedPiecesBar(Color.WHITE);
 
-		{ // Game
-			gameBoard = new GameBoard();
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(new StackPane(gameBoard, mainMenu));
+		borderPane.setTop(topBar);
+		borderPane.setBottom(bottomBar);
 
-			topBar = new CapturedPiecesBar(Color.BLACK);
-			bottomBar = new CapturedPiecesBar(Color.WHITE);
+		Scene scene = new Scene(borderPane, 600, 600);
 
-			BorderPane borderPane = new BorderPane();
+		mainMenu.setOnNewGame(event -> {
+			gameBoard.setEffect(null);
 			borderPane.setCenter(gameBoard);
-			borderPane.setTop(topBar);
-			borderPane.setBottom(bottomBar);
-
-			Scene scene = new Scene(borderPane, 600, 600);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
 			startGame(new Game());
-		}
+		});
+
+		primaryStage.setTitle("Chess");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
 	}
 
 	private void startGame(Game game){
