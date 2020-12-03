328918349a27d4b5bd3754b9089d31b03c5202da
Sofia Sinani <sofsin1208@gmail.com>
2020-12-03 15:03:16 +0100

    promotion utkast

--- src/main/java/com/example/main/App.java
+++ src/main/java/com/example/main/App.java
@@ -5,10 +5,19 @@ import com.example.game.Game;
 import com.example.ui.CapturedPiecesBar;
 import com.example.ui.GameBoard;
 import com.example.ui.MainMenu;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -21,29 +30,73 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		mainMenu = new MainMenu();
-		mainScene = new Scene(mainMenu, 750, 750);
-		mainMenu.setOnNewGame(event -> {
-			primaryStage.setScene(gameScene);
-			Game game = new Game();
-			game.setUpNormal();
-			startGame(game);
-		});
 
-		gameBoard = new GameBoard();
-		topBar = new CapturedPiecesBar(Color.BLACK);
-		bottomBar = new CapturedPiecesBar(Color.WHITE);
+		StackPane root = new StackPane();
+		JFXButton jfxButton = new JFXButton("CHOOSE PROMOTION");
+		jfxButton.setStyle("-fx-background-color: green; -fx-text-fill: white");
 
 		BorderPane borderPane = new BorderPane();
-		borderPane.setCenter(gameBoard);
-		borderPane.setTop(topBar);
-		borderPane.setBottom(bottomBar);
+		borderPane.setCenter(jfxButton);
+		root.getChildren().addAll(borderPane);
 
-		gameScene = new Scene(borderPane, 750, 750);
+		JFXDialog jfxDialog = new JFXDialog();
+		JFXDialogLayout content = new JFXDialogLayout();
+		HBox hBox = new HBox();
+		hBox.setSpacing(10);
+		Image queen = new Image("/Chess_ArtWork/Chess Symbols/Wood/QueenW.png");
+		JFXButton jfxButton1 = new JFXButton();
+		jfxButton1.setGraphic(new ImageView(queen));
+
+		Image knight = new Image("/Chess_ArtWork/Chess Symbols/Wood/KnightW.png");
+		JFXButton jfxButton2 = new JFXButton();
+		jfxButton2.setGraphic(new ImageView(knight));
+
+		Image rook = new Image("/Chess_ArtWork/Chess Symbols/Wood/RookW.png");
+		JFXButton jfxButton3 = new JFXButton();
+		jfxButton3.setGraphic(new ImageView(rook));
+
+		Image bishop = new Image("/Chess_ArtWork/Chess Symbols/Wood/BishopW.png");
+		JFXButton jfxButton4 = new JFXButton();
+		jfxButton4.setGraphic(new ImageView(bishop));
+
+		hBox.getChildren().addAll(jfxButton1, jfxButton2, jfxButton3, jfxButton4);
+		content.setBody(hBox);
+		jfxDialog.setContent(content);
+		jfxDialog.setDialogContainer(root);
+
+		Scene scene = new Scene(root, 750, 750);
+		primaryStage.setMaximized(false);
+
+		jfxButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				jfxDialog.show();
+			}
+		});
+
+//		mainMenu = new MainMenu();
+//		mainScene = new Scene(mainMenu, 750, 750);
+//		mainMenu.setOnNewGame(event -> {
+//			primaryStage.setScene(gameScene);
+//			Game game = new Game();
+//			game.setUpNormal();
+//			startGame(game);
+//		});
+//
+//		gameBoard = new GameBoard();
+//		topBar = new CapturedPiecesBar(Color.BLACK);
+//		bottomBar = new CapturedPiecesBar(Color.WHITE);
+//
+//		BorderPane borderPane = new BorderPane();
+//		borderPane.setCenter(gameBoard);
+//		borderPane.setTop(topBar);
+//		borderPane.setBottom(bottomBar);
+//
+//		gameScene = new Scene(borderPane, 750, 750);
 
 		primaryStage.getIcons().add(new Image("Chess_Artwork/Chess Pieces/Wood/KnightW.png"));
 		primaryStage.setTitle("Chess");
-		primaryStage.setScene(mainScene);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
@@ -56,6 +109,10 @@ public class App extends Application {
 		// temporary
 		game.addGameCallback(() -> gameBoard.setGame(game, Color.WHITE));
 	}
+
+	//private void choosePromotion() {
+
+	//}
 
 	public static void main(String[] args) {
 		launch(args);
