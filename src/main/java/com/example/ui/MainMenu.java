dc26f92fb5b6bcaadce6a32913b803c55dc6807e
Sofia Sinani <sofsin1208@gmail.com>
2020-12-04 11:20:36 +0100

    updated main menu class

--- src/main/java/com/example/ui/MainMenu.java
+++ src/main/java/com/example/ui/MainMenu.java
@@ -1,14 +1,17 @@
 package com.example.ui;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 
@@ -28,9 +31,9 @@ public class MainMenu extends StackPane {
 		URL url1 = getClass().getResource("/norwester/norwester.otf");
 		Font font = Font.loadFont(url1.toString(), 48);
 		chessLabel.setFont(font);
-		newGameButton = new Button("New game");
-		resumeGameButton = new Button("Resume game");
-		exitButton = new Button("Exit");
+		newGameButton = new JFXButton("New game");
+		resumeGameButton = new JFXButton("Resume game");
+		exitButton = new JFXButton("Exit");
 		Label createdByLabel = new Label("Created by 8x8");
 		createdByLabel.setStyle("-fx-text-fill: gray; -fx-font-size: 10");
 
@@ -39,15 +42,38 @@ public class MainMenu extends StackPane {
 		chessLabel.setPrefHeight(100);
 		chessLabel.setAlignment(Pos.TOP_CENTER);
 
-		newGameButton.setPrefSize(170, 40);
-		resumeGameButton.setPrefSize(170, 40);
-		exitButton.setPrefSize(170,40);
+		newGameButton.setPrefSize(200, 40);
+		resumeGameButton.setPrefSize(200, 40);
+		exitButton.setPrefSize(200,40);
+		newGameButton.setStyle("-fx-background-color: ghostwhite; -fx-text-fill: black");
+		resumeGameButton.setStyle("-fx-background-color: ghostwhite; -fx-text-fill: black");
 		exitButton.setStyle("-fx-background-color: dimgray; -fx-text-fill: black");
 
 		Font font2 = Font.loadFont(url1.toString(), 14);
 		newGameButton.setFont(font2);
 		resumeGameButton.setFont(font2);
 		exitButton.setFont(font2);
+
+		newGameButton.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				newGameButton.setCursor(Cursor.HAND);
+			}
+		});
+
+		resumeGameButton.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				resumeGameButton.setCursor(Cursor.HAND);
+			}
+		});
+
+		exitButton.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				exitButton.setCursor(Cursor.HAND);
+			}
+		});
 
 		vBox.setSpacing(10);
 		vBox.setAlignment(Pos.CENTER);
