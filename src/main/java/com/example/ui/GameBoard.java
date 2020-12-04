e0bfe3e000afb0cc88fe32219b33da9e34e92cfb
Piotr Ostrowski <piotr.ostrowski.se@gmail.com>
2020-12-04 13:55:22 +0100

    fix

--- src/main/java/com/example/ui/GameBoard.java
+++ src/main/java/com/example/ui/GameBoard.java
@@ -37,10 +37,10 @@ public class GameBoard extends StackPane {
 			}
 		}
 
-		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess Board/Wood/border_left_legend.png")), 0, 0, 1, 10);
-		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess Board/Wood/border_right.png")), 9, 0, 1, 10);
-		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess Board/Wood/border_top.png")), 1, 0, 8, 1);
-		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess Board/Wood/border_bottom_legend.png")), 1, 9, 8, 1);
+		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess_Board/Wood/border_left_legend.png")), 0, 0, 1, 10);
+		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess_Board/Wood/border_right.png")), 9, 0, 1, 10);
+		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess_Board/Wood/border_top.png")), 1, 0, 8, 1);
+		gridPane.add(new ImageView(new Image("Chess_Artwork/Chess_Board/Wood/border_bottom_legend.png")), 1, 9, 8, 1);
 
 		gridPane.setAlignment(Pos.CENTER);
 		getChildren().add(gridPane);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 open module com.example.Chess {
 	requires javafx.controls;
+	requires com.jfoenix;
 }
