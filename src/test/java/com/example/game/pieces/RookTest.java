d73e28f3feea7b7883b5dd74ee0731ad5726a744
Sofia Sinani <sofsin1208@gmail.com>
2020-12-04 12:53:15 +0100

    RookTest.java

---
+++ src/test/java/com/example/game/pieces/RookTest.java
@@ -0,0 +1,40 @@
+package com.example.game.pieces;
+
+import com.example.game.Color;
+import com.example.game.Game;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RookTest {
+
+    private Game game;
+    private Rook rook;
+
+    @BeforeAll
+    void init() {
+        game = new Game();
+        rook = new Rook(4, 4, Color.WHITE);
+        game.addPiece(rook);
+    }
+
+    @Test
+    void getPossibleMoves() {
+        assertEquals(rook.getPossibleMoves(game).size(), 14);
+    }
+
+    @Test
+    void testCanMove() {
+        for(Position possibleMovePosition : rook.getPossibleMoves(game))
+            assertTrue(rook.canMove(game, possibleMovePosition.getX(), possibleMovePosition.getY()));
+    }
+
+    @Test
+    void rookShouldNotMoveDiagonally() {
+        for(Position possibleMovePosition : rook.getPossibleMoves(game))
+            assertFalse(possibleMovePosition.isDiagonal(rook.getPosition()));
+    }
+}
